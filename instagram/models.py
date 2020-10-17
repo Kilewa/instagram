5b499b93651887ddde8c67de005d68ef6b182f58
kilewa <kilewageorge230@gmail.com>
2020-10-17 08:43:16 +0300

    created image model and users app

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Image(models.Model):
+    image = models.ImageField()
+    image_name = models.CharField(max_length=50)
+    image_caption = models.CharField(max_length=100)
+    likes = models.IntegerField()
+    comments = models.TextField()
--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Image
 
 def home(request):
+    images = Image.objects.all()
     return render(request, 'instagram/home.html')
 
 def about(request):
