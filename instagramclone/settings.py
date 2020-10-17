cb70de01f18dd6f6933b778cd6bda0e459432957
kilewa <kilewageorge230@gmail.com>
2020-10-17 09:13:00 +0300

    created user registration page

--- instagramclone/settings.py
+++ instagramclone/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'instagram',
     'bootstrap4',
+    'users',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
