423affef6405ef37cc188ea6cc947e9ffd09216d
kilewa <kilewageorge230@gmail.com>
2020-10-17 18:56:33 +0300

    added login required decorator

--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import Image
-
+@login_required
 def home(request):
     images = Image.objects.all()
     return render(request, 'instagram/home.html')
