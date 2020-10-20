29ee7990e109d666c480566f304e95affa929dd6
kilewa <kilewageorge230@gmail.com>
2020-10-20 12:47:54 +0300

    email set up

--- instagramclone/urls.py
+++ instagramclone/urls.py
@@ -24,7 +24,6 @@ urlpatterns = [
     path('', include('instagram.urls')),
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
-    # path('comment/', user_views.comment, name='comment'),
     path('edit_profile/', user_views.edit_profile, name='edit_profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
